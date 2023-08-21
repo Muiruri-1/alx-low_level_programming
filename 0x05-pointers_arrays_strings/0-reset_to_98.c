0d4a5025d1d77c44ee5d1d87f11579a4c620581f
MUiruri-1 <muirurijosphat34@gmail.com>
2023-08-21 11:23:30 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,23 +3,11 @@
 /**
  * reset_to_98 - Update the value pointed to by a pointer to 98
  * @n: Pointer to an integer
+ * Return: n
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
-/* _putchar.c */
 
-#include <unistd.h>
-#include "main.h"
-
-/**
- * _putchar - Write a character to stdout
- * @c: The character to write
- * Return: On success, return the number of characters written, otherwise -1
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
