566a5e095a24262129ffd175671ccfb3b6453488
MUiruri-1 <muirurijosphat34@gmail.com>
2023-08-21 13:56:06 -0700

    I can only go one way.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,14 @@
  */
 void print_rev(char *s)
 {
-	int length = _strlen(s);
+	if (s == NULL)
+		return;
+	int length = 0;
 
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 	for (int i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
