bb461a058b8d40cb8e283d268666fabe72d168c2
MUiruri-1 <muirurijosphat34@gmail.com>
2023-09-21 16:26:45 -0700

    singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
-#include "lists.h"
 #include <stdio.h>
 
-/**
- * print_before_main - Prints the specified lines before main is executed.
- */
-void print_before_main(void)
-{
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
+void __attribute__ ((constructor)) print_message(void);
 
+/**
+ * print_message - Prints the specified lines before main is executed.
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
 
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,16 @@
 section .data
-    message db 'Hello, Holberton', 10 ; 10 is the ASCII code for newline character
+    format db "Hello, Holberton", 10, 0 ; 10 is the ASCII code for newline, 0 for null terminator
 
 section .text
     global main
+    extern printf
 
 main:
-    ; Display the message
-    mov eax, 0        ; syscall number for sys_write
-    mov edi, 1        ; file descriptor 1 (stdout)
-    mov edx, 17       ; message length
-    syscall          ; invoke syscall
+    mov rdi, format      ; Load address of format string into rdi
+    call printf          ; Call printf function
 
     ; Exit the program
-    mov eax, 60       ; syscall number for sys_exit
-    xor edi, edi      ; exit code 0
-    syscall          ; invoke syscall
+    mov eax, 60          ; syscall number for sys_exit
+    xor edi, edi         ; exit code 0
+    syscall             ; invoke syscall
 
