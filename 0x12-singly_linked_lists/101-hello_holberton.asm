1c698083a0925788c425c7f295e68c4ffb4e21d0
MUiruri-1 <muirurijosphat34@gmail.com>
2023-09-21 16:41:30 -0700

    singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-section .data
-    format db "Hello, Holberton", 10, 0 ; 10 is the ASCII code for newline, 0 for null terminator
-
-section .text
-    global main
-    extern printf
-
+	global  main
+	extern   printf
 main:
-    mov rdi, format      ; Load address of format string into rdi
-    call printf          ; Call printf function
-
-    ; Exit the program
-    mov eax, 60          ; syscall number for sys_exit
-    xor edi, edi         ; exit code 0
-    syscall             ; invoke syscall
-
+	mov edi, format
+	xor eaX, eax
+	call printf
+	mov eax, 0
+	ret
+format: db 'Hello, Holberton\n',0
