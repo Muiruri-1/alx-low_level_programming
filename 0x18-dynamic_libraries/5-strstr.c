5d37b99840b9358c3dcf45ab139093630ec590c5
Muiruri-1 <muirurijosphat34@gmail.com>
2023-11-13 17:24:16 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
+
